efe0226622edd3c447efba4eb40c5495f0f4669c
orderpl <tobiaszperczak0@gmail.com>
2023-04-21 15:12:54 +0200

    chunks go brrrrr

--- main.py
+++ main.py
@@ -1,27 +1,54 @@
 import pygame
 from pygame import Vector2
+from pygame import transform
 from pygame.locals import *
 from sys import exit
-import mapchunk
+from mapchunk import *
 
 pygame.init()
-screen = pygame.display.set_mode((1600, 900))
+screen = pygame.display.set_mode((0, 0))
 res = pygame.math.Vector2(pygame.display.get_window_size())
 clock = pygame.time.Clock()
 
-chunk = mapchunk.MapChunk(0, (0, 0))
+seed = 69
+
+chunks:list[MapChunk] = []
+chunks.append(MapChunk(seed, (0, 0)))
+chunks.append(MapChunk(seed, (1, 0)))
+
+for chunk in chunks:
+    chunk.generate()
+
+txt_size = 48
+textures = {
+    "stone": transform.scale(pygame.image.load("stone.png").convert(), (txt_size, txt_size)),
+    "lava": transform.scale(pygame.image.load("lava.png").convert(), (txt_size, txt_size))
+}
+
+
+def draw_chunk(chunk):
+    for i1, x in enumerate(chunk.tiles):
+        for i2, y in enumerate(x):
+            rect = pygame.Rect(chunk.pos[0] * txt_size * 16 + i1 * txt_size, chunk.pos[1] * txt_size * 16 + i2 * txt_size, txt_size, txt_size)
+            if y < .5:
+                screen.blit(textures["lava"], rect)
+            else:
+                screen.blit(textures["stone"], rect)
 
 
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             exit()
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                exit()
 
         keys_pressed = pygame.key.get_pressed()
         
-        for i1, x in enumerate(chunk.tiles):
-            for i2, y in enumerate(x):
-                pygame.draw.rect(screen, [y * 255] * 3, pygame.Rect(100 + i1 * 10, 100 + i2 * 10, 10, 10))
+        for chunk in chunks:
+            if chunk.generated:
+                draw_chunk(chunk)
         
         pygame.display.update()
         screen.fill((0, 0, 0))
--- mapchunk.py
+++ mapchunk.py
@@ -1,28 +1,48 @@
 import random
 from pygame import Vector2
+from copy import deepcopy
 
 def get_perlin_noise(seed, size:tuple[int, int]):
     random.seed(seed)
     return [[random.random() for j in range(size[1])] for i in range(size[0])]
-
-
             
 neighbors = [(0, 0), (1, 0), (0, 1), (-1, 0), (0, -1)]
 
 class MapChunk:
-    def __init__(self, map_seed:int, chunk_pos:tuple[int, int]) -> None:
-        self.seed = (map_seed + chunk_pos[0] + (chunk_pos[1] >> 8)) % 2 ** 16
-        self.tiles = get_perlin_noise(self.seed, (16, 16))
-        
-    def smooth(self):
-        new_tiles = self.tiles
-        for i in range(len(self.tiles)):
-            for j in range(len(self.tiles[0])):
+    tiles:list[list[float]]
+    perlin:list[list[float]]
+    pos:tuple[int, int]
+    neighbors_noises:dict[int, list[float]]
+    
+    seed:int
+    generated:bool = False
+
+    def __init__(self, map_seed:int, pos:tuple[int, int]) -> None:
+        self.pos = pos
+        self.seed = (map_seed + pos[0] + (pos[1] >> 8)) % 2 ** 16
+        self.perlin = get_perlin_noise(self.seed, (16, 16))
+
+    def get_neighbors(self, chunklist:list):
+        for idx, chunk in enumerate(neighbors):
+            tiles = []
+            
+            self.neighbors_noises.update({idx})
+
+    def generate(self):
+        self.generated = True
+        self.tiles = deepcopy(self.perlin)
+        for i in range(2):
+            self.tiles = self.smooth(self.tiles)
+
+    def smooth(self, tiles:list[list[float]]):
+        new_tiles = tiles
+        for i in range(len(tiles)):
+            for j in range(len(tiles[0])):
                 n_sum = 0
                 for n in neighbors:
                     try:
-                        n_sum += self.tiles[i + n[0]][j + n[1]]
+                        n_sum += tiles[i + n[0]][j + n[1]]
                     except IndexError:
                         pass
                 new_tiles[i][j] = n_sum / 5
-        self.tiles = new_tiles
+        return new_tiles
