7bcb5925200812d5d061d50a7469b41602d75af1
orderpl <tobiaszperczak0@gmail.com>
2023-04-22 13:17:14 +0200

    finally

--- main.py
+++ main.py
@@ -3,23 +3,22 @@ from pygame.math import Vector2
 from pygame import transform
 from pygame.locals import *
 from sys import exit
-from mapchunk import MapChunk
+from mapchunk import MapChunk, CHUNK_SIZE
 
 pygame.init()
-screen = pygame.display.set_mode((800, 450))
+screen = pygame.display.set_mode((0, 0))
 res = pygame.math.Vector2(pygame.display.get_window_size())
 clock = pygame.time.Clock()
 
 seed = 69
 
-chunks = set()
-MapChunk(chunks, seed, (0, 0))
+chunks:set[MapChunk] = set()
 MapChunk(chunks, seed, (1, 0))
 
 for chunk in chunks:
-    chunk.step()
+    chunk.generate()
 
-txt_size = 48
+txt_size = 4
 textures = {
     "stone": transform.scale(pygame.image.load("stone.png").convert(), (txt_size, txt_size)),
     "lava": transform.scale(pygame.image.load("lava.png").convert(), (txt_size, txt_size))
@@ -29,7 +28,7 @@ textures = {
 def draw_chunk(chunk):
     for i1, x in enumerate(chunk.tiles):
         for i2, y in enumerate(x):
-            rect = pygame.Rect(chunk.pos[0] * txt_size * 16 + i1 * txt_size, chunk.pos[1] * txt_size * 16 + i2 * txt_size, txt_size, txt_size)
+            rect = pygame.Rect(chunk.pos[0] * txt_size * CHUNK_SIZE + i1 * txt_size, chunk.pos[1] * txt_size * CHUNK_SIZE + i2 * txt_size, txt_size, txt_size)
             if y < .5:
                 screen.blit(textures["lava"], rect)
             else:
@@ -48,11 +47,10 @@ while True:
         keys_pressed = pygame.key.get_pressed()
         
         for chunk in chunks:
-            if tick % 180 == 0:
-                chunk.step()
             draw_chunk(chunk)
         
         pygame.display.update()
+        pygame.display.set_caption(f"FPS: {int(clock.get_fps())}")
         screen.fill((0, 0, 0))
         delta = clock.tick(60)
         tick += 1
--- mapchunk.py
+++ mapchunk.py
@@ -7,7 +7,7 @@ def get_perlin_noise(seed, size:tuple[int, int]):
     return [[random.random() for j in range(size[1])] for i in range(size[0])]
             
 NEIGHBORS = [(0, 0), (1, 0), (0, 1), (-1, 0), (0, -1)]
-CHUNK_SIZE = 16
+CHUNK_SIZE = 128
 
 class MapChunk:
     chunks_refr:set
@@ -18,24 +18,21 @@ class MapChunk:
     seed:int
     generation_step:int = 0
     generated:bool = False
+    pos:tuple[int, int]
 
     def __init__(self, chunks, map_seed:int, pos:tuple[int, int]) -> None:
         self.chunks_refr = chunks
-        chunks.append(self)
+        chunks.add(self)
         self.pos = pos
         self.seed = (map_seed + pos[0] + (pos[1] << 8)) % 2 ** 16
         self.tiles = get_perlin_noise(self.seed, (CHUNK_SIZE, CHUNK_SIZE))
 
-    def step(self):
-        match self.generation_step:
-            case 0, 1:
-                self.smooth()
-            case 2:
-                self.smooth()
-                self.generated = True
-                
-        self.generation_step += 1
-    
+    def __repr__(self) -> str:
+        return f"""<MapChunk object>"""
+
+    def generate(self):
+        for i in range(5):
+            self.smooth()    
 
     def smooth(self):
         new_tiles = self.tiles
@@ -46,22 +43,11 @@ class MapChunk:
         
     def get_neighbors_avg(self, pos:tuple[int, int]):
         n_sum = 0
-        chunks = []
-        tiles = []
+
         for n in NEIGHBORS:
-            if (pos[0] + n[0] < 0 or pos[0] + n[0] > CHUNK_SIZE) or (pos[1] + n[1] < 0 or pos[1] + n[1] > CHUNK_SIZE):
-                chunks.append(n)
-            else:
-                chunks.append((0, 0))
-            tiles.append((n[0] + pos[0], n[1] + pos[1]))
-        
-        for chunk_idx, tile_pos in zip(chunks, tiles):
-            n_sum += self.tiles[tile_pos[0]][tile_pos[1]] if chunk_idx == (0, 0) \
-                else self.chunks_refr[self.pos[0] + chunk_idx[0]][self.pos[1] + chunk_idx[1]].tiles[tile_pos[0]][tiles[pos[1]]]
+            try:
+                n_sum += self.tiles[pos[0] + n[0]][pos[1] + n[1]]
+            except IndexError:
+                n_sum += random.random()
             
-        return n_sum / len(tiles)
-        
-    def get_neighboring_chunk(self, pos:tuple[int, int]):
-        for c in self.chunks_refr:
-            if c.pos == pos:
-                return c
+        return n_sum / len(NEIGHBORS)
